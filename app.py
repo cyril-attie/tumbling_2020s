632ebb073405c33d156580f7a39000be8019f5c7
cyril.attie <attiecyril@gmail.com>
2019-06-15 10:57:41 +0200

    Add port and host

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#home/murichun/web/CS50/bin/python3.7
+# home/murichun/web/CS50/bin/python3.7
 import os
 
 from flask import Flask, render_template, request
@@ -10,19 +10,23 @@ app = Flask(__name__)
 # engine = create_engine(os.getenv("DATABASE_URL"))
 # db = scoped_session(sessionmaker(bind=engine))
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route('/beginner_economics')
 def beginner():
     """Display Tumblien's tale"""
     return render_template('beginner_economics.html')
 
+
 @app.route('/intermediate_economics')
 def intermediate():
     """Display economic crises explanaition"""
     return render_template('intermediate_economics.html')
+
 
 @app.route('/advanced_economics')
 def advanced():
@@ -30,10 +34,12 @@ def advanced():
     return render_template('advanced_economics.html')
 
 
-
 @app.route("/about_us")
 def about():
     return render_template("about.html")
 
+
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     app.run(debug=True)
